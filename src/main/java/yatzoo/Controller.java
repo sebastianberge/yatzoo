356d39f53d28b32c5cef6ba61b9539621e2235c6
571559 <31239471+andersmh@users.noreply.github.com>
2019-02-04 12:16:12 +0100

    try catch

--- src/main/java/yatzoo/Controller.java
+++ src/main/java/yatzoo/Controller.java
@@ -23,8 +23,7 @@ public class Controller {
                 try {
                     Thread.sleep(2000);
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
+                    System.out.println("An error occurred: " + e);
                 }
                 diceCup.reset();
             }
