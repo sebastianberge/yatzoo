969358ceab6ceb5851520d4bf7014cfac40202aa
Simen Østensen <simen.ostensen@gmail.com>
2019-02-04 10:58:45 +0100

    :construction: Round.java

---
+++ src/main/java/yatzoo/Round.java
@@ -0,0 +1,24 @@
+package yatzoo;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Author: Ostensen
+*/
+
+public class Round {
+
+    private List<String> list;
+    private int current;
+
+    public Round() {
+        this.list = Arrays.asList(new String("Roll as many Lions as possible!"),
+                new String("Roll as many Snakes as possible!"), new String("Roll as many Pigs as possible!"),
+                new String("Roll as many Elephants as possible!"), new String("Roll as many Whales as possible!"),
+                new String("Roll three similar animals"), new String("Roll four similar animals"),
+                new String("Roll a House"), new String("Roll a Straight"), new String("Roll a YATZOOO!!!"));
+        this.current = 0;
+    }
+
+}
